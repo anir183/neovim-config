b5aa50974805c9ad9cb273fffef4d179d574bee5
anir183 <188557785+anir183@users.noreply.github.com>
2025-03-21 21:36:05 +0530

    feat: dont require saving for simple edits in oil

--- lua/plugins/lists/quality_of_life.lua
+++ lua/plugins/lists/quality_of_life.lua
@@ -8,6 +8,7 @@ return {
 		config = function()
 			require("oil").setup({
 				default_file_explorer = false,
+				skip_confirm_for_simple_edits = true,
 				view_options = {
 					show_hidden = true
 				}
